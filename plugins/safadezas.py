6b3670a238e3bbc29e17cb2e76bc3b3c327ba1ad
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2021-09-30 11:25:46 +0000

    update

--- plugins/safadezas.py
+++ plugins/safadezas.py
@@ -14,7 +14,7 @@ async def hor_(message: Message):
     reply_ = message.reply_to_message
     if not reply_:
         iam = f"🔥 I am {random.choice(range(0,100))}% horny!"
-        await message.edit(hor_)
+        await message.edit(hor)
         return
     user_ = await kannax.get_users(reply_.from_user.id)
     msg_ = f" 🔥{user_.mention} é {random.choice(range(0,100))}% horny!"
@@ -22,22 +22,22 @@ async def hor_(message: Message):
 
 
 @kannax.on_cmd("clock", about={"header": "My clock size is!?"})
-async def clock_(message: Message):
+async def cl_(message: Message):
     reply_ = message.reply_to_message
     if not reply_:
         iam = f"🍆 My clock size is {random.choice(range(0,100))}cm!"
-        await message.edit(clock_)
+        await message.edit(cl)
         return
     user_ = await kannax.get_users(reply_.from_user.id)
     msg_ = f" 🍆{user_.mention} My clock size is {random.choice(range(0,100))}cm!"
     await message.edit(msg_)
 
 @kannax.on_cmd("cute", about={"header": "i am  cute!"})
-async def cute_(message: Message):
+async def cut_(message: Message):
     reply_ = message.reply_to_message
     if not reply_:
         iam = f"🍑 I am {random.choice(range(0,100))}% cute!"
-        await message.edit(cute_)
+        await message.edit(cut)
         return
     user_ = await kannax.get_users(reply_.from_user.id)
     msg_ = f" 🍑 I am {user_.mention}  {random.choice(range(0,100))}% cute!"
