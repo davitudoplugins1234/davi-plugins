4e7596e10b8567e69319beffa1ee0fa66f5aad09
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 12:04:46 -0300

    Update wtf.py

--- plugins/wtf.py
+++ plugins/wtf.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("w", about={"header": "wtf"}, trigger="", allow_via_bot=False)
+@kannax.on_cmd("w", about={"header": "wtf"}
 async def w_(message: Message):
     out_str = f"wtff"
     await message.edit(out_str)
