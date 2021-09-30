cb8bbcac56615a961ebd65832641fc47f9f7eca0
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2021-09-30 13:22:23 +0000

    update

--- plugins/beleza.py
+++ plugins/beleza.py
@@ -11,9 +11,9 @@ from kannax import Message, kannax
 async def beleza_(message: Message):
     reply_ = message.reply_to_message
     if not reply_:
-        beleza = f" Eu sou{random.choice(range(0,100))}% bonito!"
+        beleza = f"Eu sou {random.choice(range(0,100))}% bonito 😍!"
         await message.edit(beleza)
         return
     user_ = await kannax.get_users(reply_.from_user.id)
-    msg_ = f" {user_.mention} é {random.choice(range(0,100))}% bonito!"
+    msg_ = f" {user_.mention} é {random.choice(range(0,100))}% bonito! 😍"
     await message.edit(msg_)
