7574aa1b9e300df39a82ac01b106e456d899a9ca
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 12:43:13 -0300

    Update sorte.py

--- plugins/sorte.py
+++ plugins/sorte.py
@@ -1,8 +1,8 @@
-from random import randint, choice
+import random
 
 from kannax import Message, kannax
 
-@kannax.on_cmd('sor' , about={"header": 'estou com sorte?'})
+@kannax.on_cmd('sort' , about={"header": 'estou com sorte?'})
 async def sort_(message: Message):
     sort = f"Número da sorte: {random.choice(range(0, 10))}"
     await message.edit(sort)
