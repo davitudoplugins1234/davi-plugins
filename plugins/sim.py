026e08a9474df1c5f699138ce8bee68f61028668
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 09:03:29 -0300

    Update sim.py

--- plugins/sim.py
+++ plugins/sim.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("s", about={"header": "sim"})
+@kannax.on_cmd("s", about={"header": "sim"}, trigger="", allow_via_bot=False)
 async def s_(message: Message):
     out_str = f"simm"
     await message.edit(out_str)
