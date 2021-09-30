73a55e73d7b023a8b076065fa1f7571dc10f017d
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 06:58:16 -0300

    Create sei_lá.py

---
+++ plugins/sei_lá.py
@@ -0,0 +1,12 @@
+# by joaoppierri
+
+from pyrogram.errors import BadRequest
+
+from kannax import Message, kannax
+from kannax.utils import get_file_id
+
+@kannax.on_cmd("se", about={"header": "sei lá"})
+async def se_(message: Message):
+    out_str = f"sei lá"
+    await message.edit(out_str)
+    
