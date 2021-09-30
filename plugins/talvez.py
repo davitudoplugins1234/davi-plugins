892bbb9e7c522471c31d7d3c4709d939b12e1826
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 09:03:15 -0300

    Update talvez.py

--- plugins/talvez.py
+++ plugins/talvez.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("t", about={"header": "talvez 🤔"})
+@kannax.on_cmd("t", about={"header": "talvez 🤔"}, trigger="", allow_via_bot=False)
 async def t_(message: Message):
     out_str = f"talvez 🤔"
     await message.edit(out_str)
