5445ef65130efea1ff2dfa44c0f2e4297180026f
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 12:12:06 -0300

    Update td_bom.py

--- plugins/td_bom.py
+++ plugins/td_bom.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("td", about={"header": "tudo bom?"}
+@kannax.on_cmd("td", about={"header": "tudo bom?"})
 async def td_(message: Message):
     out_str = f"tudo bom?"
     await message.edit(out_str)
