bd0d55e70a892006748ffd820c51f2377d9d6d1b
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2021-09-30 09:58:10 +0000

    update

--- plugins/sim.py
+++ plugins/sim.py
@@ -7,6 +7,6 @@ from kannax.utils import get_file_id
 
 @kannax.on_cmd("s", about={"header": "sim"})
 async def s_(message: Message):
-    out_str = f"simm"
+    out_str = f"sim"
     await message.edit(out_str)
     
