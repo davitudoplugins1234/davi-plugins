c99509cc2f78d8d117391ce18d11d7cb8ffd6cc1
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-09-30 12:02:45 -0300

    Update não.py

--- plugins/não.py
+++ plugins/não.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("n", about={"header": "não"}, trigger="", allow_via_bot=False)
+@kannax.on_cmd("n", about={"header": "não"}
 async def n_(message: Message):
     out_str = f"não"
     await message.edit(out_str)
