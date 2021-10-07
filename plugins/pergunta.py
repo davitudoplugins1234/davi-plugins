2ca63df64daf55a00a946b8bab8743e25bc2752d
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2021-10-07 05:29:36 +0000

    update

--- plugins/pergunta.py
+++ plugins/pergunta.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("pergu", about={"header": "ask telegram"}, allow_via_bot=False)
+@kannax.on_cmd("perg", about={"header": "ask telegram"}, allow_via_bot=False)
 async def perg_(message: Message):
     out_str f"https://telegra.ph/Como-fazer-perguntas-no-Telegram-07-21"
     await message.edit(out_str)
