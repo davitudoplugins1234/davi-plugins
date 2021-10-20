731462e99d1151c87490e704b4556901327fb15f
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-10-20 09:51:19 -0300

    create xingamentos.py

---
+++ plugins/xingamentos.py
@@ -0,0 +1,42 @@
+from kannax import Message, kannax
+      
+
+@kannax.on_cmd("(Crl|crl)$", about={"header": "caralho"}, trigger="", allow_via_bot=False)
+async def crl_(message: Message):
+    """crl"""
+    await message.edit("caralho")
+
+
+@kannax.on_cmd("Vsf|vsf", about={"header": "vai se foder"})
+async def vsf_(message: Message):
+    reply_ = message.reply_to_message
+    if not reply_:
+        vsf = f"vai se foder"
+        await message.edit(vsf)
+        return
+    user_ = await kannax.get_users(reply_.from_user.id)
+    msg_ = f" {user_.mention} vai tomar no seu cu"
+    await message.edit(msg_)
+
+
+@kannax.on_cmd("Pqp|pqp", about={"header": "puta que pariu"})
+async def pqp_(message: Message):
+    reply_ = message.reply_to_message
+    if not reply_:
+        pqp = f"puta que pariu"
+        await message.edit(pqp)
+        return
+    user_ = await kannax.get_users(reply_.from_user.id)
+    msg_ = f" {user_.mention} vai pra puta que pariu"
+    await message.edit(msg_)
+
+@kannax.on_cmd("Crl|crl", about={"header": "caralho"})
+async def crl_(message: Message):
+    reply_ = message.reply_to_message
+    if not reply_:
+        crl = f"caralho"
+        await message.edit(crl)
+        return
+    user_ = await kannax.get_users(reply_.from_user.id)
+    msg_ = f" {user_.mention} vai pra casa do caralho"
+    await message.edit(msg_)
