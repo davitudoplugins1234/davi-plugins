ef06bd3cef92271789e25b46b9ee03351ac99e5a
João Pedro <58857531+JoaoPedroPierri@users.noreply.github.com>
2021-10-20 13:11:39 -0300

    Update repo.py

--- plugins/repo.py
+++ plugins/repo.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("repo", about={"header": "customrepo"}, allow_via_bot=False)
-async def repo_(message: Message):
+@kannax.on_cmd("joao", about={"header": "customrepo"}, allow_via_bot=False)
+async def joao_(message: Message):
     out_str = f"https://github.com/JoaoPedroPierri/joao-plugins"
     await message.edit(out_str)
