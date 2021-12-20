85458e2713e0a76937c5f68675394a42bed3cb19
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2021-12-20 11:18:08 -0300

    Update melhor.py

--- plugins/melhor.py
+++ plugins/melhor.py
@@ -9,7 +9,7 @@ from kannax import Message, kannax
 @kannax.on_cmd("melhor", about={"header": "Melhor"})
 async def sii_(message: Message):
     out_str =   f"""
-    Eu sou o melhor, posso não ser mas em minha cabeça eu sou o melhor
+    Eu sou o melhor posso não ser mas, em minha cabeça eu sou o melhor
 ⠀⠀⠀⠀⠀⢀⣠⠤⡶⣲⢺⣴⣶⢭⣉⢲⣀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⢀⡾⢵⣶⣿⣿⣿⣾⣷⣳⣿⣷⣵⣈⠷⢤⡀⠀⠀⠀⠀⠀
 ⠀⠀⠘⢾⣿⡿⠿⠿⠿⠿⠿⠿⢿⡿⣿⣿⣿⣾⣾⣦⠀⠀⠀⠀
