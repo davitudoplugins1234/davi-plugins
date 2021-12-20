514eea8462c4d1f908881f3e3257c69cc93d7510
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2021-12-20 00:57:05 -0300

    Update and rename waifu.py to gostosa.py

--- plugins/gostosa.py
+++ plugins/gostosa.py
@@ -4,7 +4,7 @@ import asyncio
 
 from kannax import Message, kannax
 
-@kannax.on_cmd("waifu", about={"header": "Waifu Gostosa"})
+@kannax.on_cmd("gostosa", about={"header": "Waifu Gostosa"})
 async def cr7_(message: Message):
   out_str = f"""
 ⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄ 
