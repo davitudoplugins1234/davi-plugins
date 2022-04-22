399c45c1d4143b6ef3d8b91a3ec3006361529788
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2022-04-22 18:41:50 +0000

    removed modulos

--- plugins/sorte.py
+++
@@ -1,8 +0,0 @@
-import random
-
-from kannax import Message, kannax
-
-@kannax.on_cmd('sort' , about={"header": 'estou com sorte?'})
-async def sort_(message: Message):
-    sort = f"Número da sorte: {random.choice(range(0, 10))}"
-    await message.edit(sort)
