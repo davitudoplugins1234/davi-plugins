3f990eb340461b027190206ba3494d3c59f511ec
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2022-04-22 18:34:51 +0000

    padrão

--- plugins/risadas.py
+++ plugins/risadas.py
@@ -5,7 +5,7 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("K|k", trigger="", about={"header": "risada curta"}, allow_via_bot=False)
+@kannax.on_cmd("K|k", about={"header": "risada curta"}, allow_via_bot=False)
 async def k_(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
