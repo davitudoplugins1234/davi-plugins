65279f19c3b01d968bd5dffb142f65033e6800b6
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2022-04-22 18:15:01 +0000

    updates

--- plugins/risadas.py
+++ plugins/risadas.py
@@ -5,13 +5,13 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("k", about={"header": "risada mais curta"}, allow_via_bot=False)
+@kannax.on_cmd("K|k", about={"header": "risada curta"}, trigger="", allow_via_bot=False)
 async def k_(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
 
 
-@kannax.on_cmd("kkk", about={"header": "risada muito longa"}, allow_via_bot=False)
+@kannax.on_cmd("KKK|kkk", about={"header": "risada  longa"}, allow_via_bot=False)
 async def kkk_(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
