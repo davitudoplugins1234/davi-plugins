67b7d52f53d3cfd1e1a5827c81399ea0ff68847f
JoaoPedroPierri <joaopedropierri2002@gmail.com>
2022-04-23 07:37:17 +0000

    update

--- plugins/memes.py
+++ plugins/memes.py
@@ -5,18 +5,15 @@ from pyrogram.errors import BadRequest
 from kannax import Message, kannax
 from kannax.utils import get_file_id
 
-@kannax.on_cmd("K|k)$", about={"header": "KKKKKKKKKKKKKKKK"}, trigger="", allow_via_bot=False)
+@kannax.on_cmd("k", about={"header": "risada curta"}, allow_via_bot=False)
 async def k_(message: Message):
-    """KK"""
-    K = "K "
-    for _ in range(20):
-         K = K[:-1] + "K"
-    await message.edit(K)
+    out_str =f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+    await message.edit(out_str)
 
 
 @kannax.on_cmd("kkk", about={"header": "risada  longa"}, allow_via_bot=False)
 async def kkk_(message: Message):
-    out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
+    out_str = f"KKKKKKKKKKKKKkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
     
 
