fb8acd37f4e60f836d02832f6473853f542f79a4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-07 16:33:34 -0300

    Update __main__.py

--- plugins/meme/meme/__main__.py
+++ plugins/meme/meme/__main__.py
@@ -13,3 +13,23 @@ async def k_(message: Message):
 async def kkk_(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
+
+@userge.on_cmd(
+    "clima",
+    about={
+        "header": "Weather of the city",
+        "description": "Busque o clima para uma cidade",
+        "usage": "{tr}clima [text]",
+    },
+)
+async def clima_ln(message: Message):
+    city_ = message.input_str
+    bot_ = "@WhiterKangBOT"
+    async with userge.conversation(bot_, timeout=1000) as conv:
+        try:
+            await conv.send_message(f"!clima {city_}")
+        except YouBlockedUser:
+            await message.reply("Por Favor desbloqueie a @WhiterKangBOT")
+            return
+        response = await conv.get_response(mark_read=False)
+        await message.edit(response.text)
