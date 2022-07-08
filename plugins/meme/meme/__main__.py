6c00fcec357b82c1785020fd8785b599fa7955b1
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-07 23:11:28 -0300

    Update __main__.py

--- plugins/meme/meme/__main__.py
+++ plugins/meme/meme/__main__.py
@@ -3,12 +3,12 @@
 
 from userge import Message, userge
 
-@userge.on_cmd("k|K", about={"header": "risada curta"}, allow_via_bot=False)
+@userge.on_cmd("k", about={"header": "risada curta"}, allow_via_bot=False)
 async def k_(message: Message):
     out_str =f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
 
-@userge.on_cmd("kkk|KKK", about={"header": "risada  longa"}, allow_via_bot=False)
+@userge.on_cmd("kkk", about={"header": "risada  longa"}, allow_via_bot=False)
 async def kkk_(message: Message):
     out_str = f"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK"
     await message.edit(out_str)
