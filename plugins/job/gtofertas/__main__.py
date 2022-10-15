2b1947a9b73fee50d9df69364049220023117092
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-10-15 10:18:17 -0300

    Update __main__.py

--- plugins/job/gtofertas/__main__.py
+++ plugins/job/gtofertas/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from userge import userge, Message
@@ -15,6 +16,7 @@ async def gtofertas_(message: Message):
             if i:
                  message_count += 1
                  await message.edit(f"<b>Ofertas Enviadas</b>: <i>{message_count}</i>\n\n<b>Time sleep</b>: <i>{time}</i>\n<b>ID</b>: <code>{message_reply}</code>")
+                 await asyncio.sleep(int(time))
         except MessageIdInvalid:
             message_reply += 1
             continue
