28914f0d82d7b6c43096e983dba97b1787a8788f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-11-25 16:59:33 -0300

    @BubbalooTeam

--- plugins/job/gtofertas/__main__.py
+++ plugins/job/gtofertas/__main__.py
@@ -5,16 +5,17 @@ from userge import userge, Message
 
 @userge.on_cmd("gtofertas", about={"header": "Enviar ofertas"}, allow_via_bot=False)
 async def gtofertas_(message: Message):
-    id, time = message.filtered_input_str.split(maxsplit=1)
+    id, time, max_messages = message.filtered_input_str.split(maxsplit=1)
     message_reply = int(id)
     message_count = 0
-    while True:
+    async for message_count in range(max_messages):
         try:
+            if message_count >= max_messages:
+                break
             i = await userge.forward_messages(-1001115033767, -1001197236241, message_reply)
             message_reply += 1
             if i:
-                 message_count += 1
-                 await message.edit(f"<b>Ofertas Enviadas</b>: <i>{message_count}</i>\n\n<b>Time sleep</b>: <i>{time}</i>\n<b>ID</b>: <code>{message_reply}</code>")
+                 await message.edit(f"<b>Ofertas Enviadas</b>: <i>{message_count}/{max_messages}</i>\n\n<b>Time sleep</b>: <i>{time}</i>\n<b>ID</b>: <code>{message_reply}</code>")
                  await asyncio.sleep(int(time))
         except MessageIdInvalid:
             message_reply += 1
